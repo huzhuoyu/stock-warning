3893f244bf7366cf2255fe47529d4d5ae8e6e2f4
呼卓宇 <652355283@qq.com>
2022-04-20 17:22:37 +0800

    pom依赖回滚

--- stock-warning-admin/src/main/java/com/hzy/StockWarningApplication.java
+++ stock-warning-admin/src/main/java/com/hzy/StockWarningApplication.java
@@ -1,6 +1,5 @@
 package com.hzy;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
